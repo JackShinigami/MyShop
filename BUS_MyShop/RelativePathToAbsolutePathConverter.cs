e24a2afaff99012e13b6548f81a2d4879eee0f40
Biện Công Khanh <21120264@student.hcmus.edu.vn>
2023-12-08 17:45:58 +0700

    Update login window

---
+++ BUS_MyShop/RelativePathToAbsolutePathConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Data;
+
+namespace BUS_MyShop {
+    public class RelativePathToAbsolutePathConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            string? relativePath = value as string;
+            if (relativePath == null)
+                return "";
+            // check if it is already an absolute path
+            if (Path.IsPathRooted(relativePath))
+                return relativePath;
+            string absolutePath = Path.GetFullPath(relativePath);
+            return absolutePath;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            string? absolutePath = value as string;
+            if (absolutePath == null)
+                return "";
+
+            string relativePath = Path.Combine("Images", Path.GetFileName(absolutePath));
+            // copy image to 'Images' folder
+            Debug.WriteLine(relativePath);
+            // check if exist already, do nothing
+            if (File.Exists(relativePath))
+                return relativePath;
+
+            File.Copy(absolutePath, relativePath, true);
+            return relativePath;
+        }
+    }
+
+}
---
+++ GUI_MyShop/LoginWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using DocumentFormat.OpenXml.Drawing.Wordprocessing;
+using DocumentFormat.OpenXml.Vml.Office;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BUS_MyShop;
+
+namespace GUI_MyShop {
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window {
+        private Point startPoint;
+        private bool isDragging = false;
+        Rect restoreSize = new Rect(0, 0, 0, 0);
+
+
+        public LoginWindow() {
+            InitializeComponent();
+            StateChanged += LoginWindow_StateChanged;
+
+        }
+
+        #region Title Bar Event Handler
+
+
+        private void LoginWindow_StateChanged(object? sender, EventArgs e) {
+            if (WindowState == WindowState.Normal) {
+
+            } else if (WindowState == WindowState.Maximized) {
+
+            } else if (WindowState == WindowState.Minimized) {
+
+            }
+
+            // fade in the window
+            DoubleAnimation animation = new DoubleAnimation(1, (Duration)TimeSpan.FromSeconds(0.1));
+            this.BeginAnimation(OpacityProperty, animation);
+
+
+            Debug.WriteLine(WindowState);
+        }
+
+
+        private void closeButton_Click(object sender, RoutedEventArgs e) {
+            // fade out the window
+            DoubleAnimation animation = new DoubleAnimation(0, (Duration)TimeSpan.FromSeconds(0.2));
+            animation.Completed += (s, _) => this.Close();
+            this.BeginAnimation(TopProperty, new DoubleAnimation(500, (Duration)TimeSpan.FromSeconds(0.2)));
+            this.BeginAnimation(OpacityProperty, animation);
+        }
+
+        private void maximizeButton_Click(object sender, RoutedEventArgs e) {
+            // maximize the window with animation 
+
+            // fade out the window
+            DoubleAnimation animation = new DoubleAnimation(0.0, (Duration)TimeSpan.FromSeconds(0.1));
+            this.BeginAnimation(OpacityProperty, animation);
+
+            Task.Delay(200).ContinueWith(_ => {
+                Dispatcher.Invoke(() => {
+                    if (WindowState == WindowState.Maximized) {
+                        WindowState = WindowState.Normal;
+                    } else {
+                        WindowState = WindowState.Maximized;
+                    }
+                });
+            });
+        }
+
+
+
+        private void minimizeButton_Click(object sender, RoutedEventArgs e) {
+           
+            Task.Delay(300).ContinueWith(_ => {
+                Dispatcher.Invoke(() => {
+                    WindowState = WindowState.Minimized;
+                });
+            });
+
+        }
+
+        private void titleBarBorder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            if (e.ClickCount == 2) {
+                maximizeButton_Click(sender, e);
+                return;
+            }
+
+            if (WindowState == WindowState.Maximized) {
+                startPoint = e.GetPosition(this);
+                isDragging = true;
+            } else {
+                DragMove();
+            }
+        }
+
+
+        private void titleBarBorder_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            isDragging = false;
+        }
+        private void titleBarBorder_MouseMove(object sender, MouseEventArgs e) {
+
+            if (isDragging) {
+                if (WindowState == WindowState.Maximized) {
+                    WindowState = WindowState.Normal;
+                    Left = startPoint.X - RestoreBounds.Width / 2;
+                    Top = startPoint.Y - 10;
+                }
+            }
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                DragMove();
+            }
+        }
+
+        private void titleBarBorder_MouseLeave(object sender, MouseEventArgs e) {
+            isDragging = false;
+        }
+
+
+
+        #endregion
+
+        private void loginButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
