31c11077101a7b45fa2cb31091c73d04fbebb285
JackShinigami <quangkhanh21112003@gmail.com>
2024-01-05 12:02:40 +0700

    update customer

--- GUI_MyShop/Customers.xaml.cs
+++ GUI_MyShop/Customers.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using BUS_MyShop;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DTO_MyShop;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,26 @@ namespace GUI_MyShop
     /// </summary>
     public partial class Customers : UserControl
     {
+
+        BUS_Customers bus = BUS_Customers.Instance;
+        BindingList<Customer> customers;
+        private int _currentPage = 1;
+        private int _pageSize = 5;
+        private int _totalPage = 0;
+        private int _totalRecord = 0;
+
         public Customers()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+            deleteButton.IsEnabled = false;
+            editButton.IsEnabled = false;
+
+            this.DataContext = this;
         }
 
         private void addButton_Click(object sender, RoutedEventArgs e)
@@ -43,6 +64,122 @@ namespace GUI_MyShop
         private void searchButton_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void previousPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            _currentPage--;
+            currentPageTextBox.Text = _currentPage.ToString();
+            LoadData();
+        }
+
+        private void nextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            _currentPage++;
+            currentPageTextBox.Text = _currentPage.ToString();
+            LoadData();
+        }
+
+        private void logicTransmitPageSizeToLoadBooks()
+        {
+            try
+            {
+                int input = int.Parse(pageSizeTextBox.Text);
+                if (1 <= input && input <= 100)
+                {
+                    LoadData();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                // do nothing
+            }
+            pageSizeTextBox.Text = _pageSize.ToString();
+        }
+
+        private void reloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            logicTransmitPageSizeToLoadBooks();
+        }
+
+        private void changePageSize_KeyDowm(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                logicTransmitPageSizeToLoadBooks();
+            }
+        }
+
+        private void changeCurrentPage_KeyDowm(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                try
+                {
+                    int input = int.Parse(currentPageTextBox.Text);
+                    if (1 <= input && input <= _totalPage)
+                    {
+                        _currentPage = input;
+                        LoadData();
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // do nothing
+                }
+                currentPageTextBox.Text = _currentPage.ToString();
+            }
+        }
+
+        private void LoadData()
+        {
+
+            int oldPageSize = _pageSize;
+            int oldCurrentPage = _currentPage;
+            try
+            {
+                _pageSize = int.Parse(pageSizeTextBox.Text);
+                _currentPage = int.Parse(currentPageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                _pageSize = oldPageSize;
+                _currentPage = oldCurrentPage;
+                pageSizeTextBox.Text = _pageSize.ToString();
+                currentPageTextBox.Text = _currentPage.ToString();
+            }
+
+
+            if (oldPageSize != _pageSize)
+            {
+                _currentPage = 1;
+                currentPageTextBox.Text = _currentPage.ToString();
+            }
+            else if (oldCurrentPage != _currentPage)
+            {
+                currentPageTextBox.Text = _currentPage.ToString();
+            }
+
+            int count = bus.GetCount();
+            customers = bus.GetCustomers((_currentPage - 1) * _pageSize, _pageSize);
+            dataGrid_Customers.ItemsSource = customers;
+
+            if (count != _totalRecord)
+            {
+                _totalRecord = count;
+                _totalPage = _totalRecord / _pageSize + (_totalRecord % _pageSize == 0 ? 0 : 1);
+                totalPageLabel.Content = _totalPage;
+            }
+            if (oldPageSize != _pageSize)
+            {
+                _totalPage = _totalRecord / _pageSize + (_totalRecord % _pageSize == 0 ? 0 : 1);
+                totalPageLabel.Content = _totalPage;
+            }
+
+            previousPageButton.IsEnabled = _currentPage > 1;
+            nextPageButton.IsEnabled = _currentPage < _totalPage;
         }
     }
 }
